db5d655ac90ec114a261edd46583ff0d103da2ee
Francis Lalnunmawia <franzyke@gmail.com>
2022-07-16 18:26:55 +0530

    custom menu structure

--- src/Site.php
+++ src/Site.php
@@ -2,6 +2,12 @@
 
 namespace Lailen\OpenAdmin\Site;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
+use OpenAdmin\Admin\Auth\Database\Menu;
 use OpenAdmin\Admin\Extension;
 
 class Site extends Extension
@@ -12,41 +18,58 @@ class Site extends Extension
 
     public $assets = __DIR__.'/../resources/assets';
 
-    public $menu = [
-        'title' => 'Site',
-        'path'  => 'site',
-        'icon'  => 'icon-cogs',
-        'children' => [
+    public static function import()
+    {
+        $lastOrder = Menu::max('order');
+
+        $menus = [
             [
                 'title' => 'Menu',
-                'path'  => 'menus',
+                'uri'  => 'menus',
                 'icon'  => 'icon-bars',
             ],
             [
                 'title' => 'Pages',
-                'path'  => 'pages',
+                'uri'  => 'pages',
                 'icon'  => 'icon-file',
             ],
             [
                 'title' => 'Gallery',
-                'path'  => 'albums',
+                'uri'  => 'albums',
                 'icon'  => 'icon-images',
             ],
             [
                 'title' => 'Posts',
-                'path'  => 'posts',
+                'uri'  => 'posts',
                 'icon'  => 'icon-file-alt',
             ],
             [
                 'title' => 'Tags',
-                'path'  => 'post-tags',
+                'uri'  => 'post-tags',
                 'icon'  => 'icon-tags',
             ],
             [
                 'title' => 'Categories',
-                'path'  => 'post-categories',
+                'uri'  => 'post-categories',
                 'icon'  => 'icon-file',
             ],
-        ]
-    ];
+        ];
+
+        foreach ($menus as $menu) {
+            $menu['parent_id'] = 0;
+            $menu['order'] = $lastOrder++;
+
+            Menu::create($menu);
+        }
+
+        Artisan::call('admin:import', ['extension' => 'config']);
+        Artisan::call('vendor:publish', ['tag' => 'open-admin-ckeditor']);
+
+        Artisan::call('migrate');
+
+        /**
+         * Mamawh hun atan. open-admin-ext helpers ami ka copy
+         */
+        // parent::createPermission('Lailen Site Management', 'ext.site', 'site/*');
+    }
 }
