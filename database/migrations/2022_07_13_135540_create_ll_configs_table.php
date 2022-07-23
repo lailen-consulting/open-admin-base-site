8dfd7c6b9d5502a5112a28135e09ed63a7c2d07b
Francis Lalnunmawia <franzyke@gmail.com>
2022-07-23 17:31:49 +0530

    include samples in migration
    cant load seeder for some reason

--- database/migrations/2022_07_13_135540_create_ll_configs_table.php
+++ database/migrations/2022_07_13_135540_create_ll_configs_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -22,6 +23,56 @@ return new class extends Migration
             $table->longText('options')->nullable();
             $table->timestamps();
         });
+
+        /**
+         * Seeder namespace a dik thei lo
+         */
+        DB::table('ll_menus')->insert([
+            'name' => 'Main Menu',
+            'slug' => 'main-menu',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('ll_menu_items')->insert([
+            'title' => 'Home',
+            'link' => '/',
+            'menu_id' => 1,
+            'parent_id' => 0,
+            'order' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('ll_pages')->insert([
+            'title' => 'About Us',
+            'slug' => 'about-us',
+            'excerpt' => 'Excerpt',
+            'content' => 'Content',
+            'user_id' => 1,
+            'published_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('ll_configs')->insert([
+            [
+                'title' => 'Main Navigation',
+                'key' => 'mainNavigation',
+                'value' => 1,
+                'type' => 'single-menu',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Site Title',
+                'key' => 'siteTitle',
+                'value' => 'Lailen Site',
+                'type' => 'text',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 
     /**
